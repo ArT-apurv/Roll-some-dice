eb17b34b70ce0760eb4a3317bf7c6727dabb6101
ArT-apurv <89764232+ArT-apurv@users.noreply.github.com>
2021-09-23 18:48:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ document.querySelector('.btn--roll').addEventListener('click', function () {
   dice.classList.remove('hidden');
   var randomNumber = Math.trunc(Math.random() * 6) + 1;
   var imgSource =
-    'https://art-apurv.github.io/piggame/dice-' + randomNumber + '.png';
+    'https://art-apurv.github.io/Roll-some-dice/-' + randomNumber + '.png';
   dice.src = imgSource;
 
   if (score[i] < 20) {
